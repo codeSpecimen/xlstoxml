e257822bc8ccb5f771bc2f29f0f944d1a0d9d98c
Ray Ibon <ribon@standard-insurance.com>
2023-01-17 05:19:07 +0800

    fix errors removed import unused

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import { useEffect, useState } from "react";
 import './App.css';
-import { json2xml } from 'xml-js';
-import { toXML } from 'jstoxml';
 
 function App() {
   const [xml, setXml] = useState()
